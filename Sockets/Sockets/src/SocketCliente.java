ef1251669bbd94c2196e06b065d5028f95d5f5ce
Martin <musicanumero01@gmail.com>
2022-12-15 17:52:36 +0000

    Borré esta carpeta de la practica de sistema Sockets por incompleta,  la que si está completa de Sockets es la que está en la carpeta NetBeans prácticas del curso

--- Sockets/Sockets/src/SocketCliente.java
+++
@@ -1,9 +0,0 @@
-public class SocketCliente {
-    
-    public SocketCliente() {
-    }
-
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
-}
